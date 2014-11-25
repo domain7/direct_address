cd4e34cd838c293595626d9bf113f45d1afda13c
Shane Davies <shane@domain7.com>
2014-11-25 15:07:51 -0800

    Fix geoname

--- lib/direct_address/harvester/geoname.rb
+++ lib/direct_address/harvester/geoname.rb
@@ -1,34 +1,34 @@
 class Geoname
-	
+
 	class InvalidModelError < StandardError; end
-	
+
 	def self.retrieve_to_db
 		raise(InvalidModelError, 'Country and Region models must be generated before populating the database. Please make sure the gem is installed into a rails project then try running: script/generate direct_address') if !gem_integrated?
 		countries = YAML.load_file(File.join(File.dirname(__FILE__), 'countries.yml'))
 		for country in countries
 			abbrev = country[0]
 			name = country[1]
-			c = Country.find_or_create_by_abbreviation(:abbreviation => abbrev, :name => name)
-			regions = YAML.load_file(File.join(File.dirname(__FILE__), 'regions', "#{abbrev}.yml")) rescue [] 
+			c = Country.where(abbreviation: abbrev).first_or_create(name: name)
+			regions = YAML.load_file(File.join(File.dirname(__FILE__), 'regions', "#{abbrev}.yml")) rescue []
 			for region in regions
 				# this stopped working - using the next line instead
 				# r = c.regions.find_or_create_by_name(:name => region)
-				r = Region.find_or_create_by_name_and_country_id(:name => region, :country_id => c.id)
+				Region.where(name: region, country_id: c.id).first_or_create
 				puts "Created country / region: #{name} / #{region}"
 			end
 		end
 	end
 
 private
-	
+
 	def self.gem_integrated?
 		begin
-			c = Country.new(:name => 'test', :abbreviation => 't')
-			r = Region.new(:name => 'test', :country_id => -1)
+			Country.new(:name => 'test', :abbreviation => 't')
+			Region.new(:name => 'test', :country_id => -1)
 			true
 		rescue
 			false
 		end
 	end
-	
+
 end
